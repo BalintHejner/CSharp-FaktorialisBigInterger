b8b8afdf8d8f57352ef33b820a6de53fe4db78e5
Bálint Hejner <hejnerbalint@gmail.com>
2020-09-30 12:01:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace faktoriális2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("faktoriális meghatározása 20 és 40 között");
+            for (int szám = 20; szám <= 40; szám++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szám; i++) 
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{ szám}!={ faktor}");
+            }
+            Console.ReadKey();
+        }
+        
+    }   
+}
+    
